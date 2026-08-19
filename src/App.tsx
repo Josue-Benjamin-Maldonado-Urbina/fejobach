1d58d5071d69ea4d0425e679a91932d8c7dad8f1
Josue-Benjamin-Maldonado-Urbina <jb.maldonado.u@gmail.com>
2026-08-19 00:47:41 -0600

    comming soon

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,5 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./views/Home/Home";
-import MisionVision from "./components/Section/MisionVision";
-import Objetivos from "./components/Section/Objectives";
-import Calendario from "./components/Section/Calendar";
-import Resources from "./components/Section/Resources";
-import Alabanza from "./components/Section/resources/Alabanza";
-import Predicacion from "./components/Section/resources/Predicacion";
-import Evangelismo from "./components/Section/resources/Evangelismo";
-import Multimedia from "./components/Section/resources/Multimedia";
-import DeportesSociales from "./components/Section/resources/DeportesSociales";
-import Activities from "./components/Section/Activities";
-import Meet from "./components/Section/Meet";
-import ActivitiesPast from "./components/Section/ActivitiesPast";
-import PdfSection from "./screens/PdfSection";
-import Campamento from "./views/Campamento/Campamento";
-import CMS2026 from "./views/Campamento/LandingPage";
-import Notas from "./views/Campamento/Notas";
+
 import RegistroTorneo from "./components/Section/RegistroTorneo";
 import UnderConstruction from "./components/Section/UnderConstruction";
 
@@ -25,22 +8,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<UnderConstruction />} />
-        {/* <Route path="/mision-vision" element={<MisionVision />} />
-        <Route path="/" element={<Objetivos />} />
-        <Route path="/activities" element={<Activities />} />
-        <Route path="/calendar" element={<Calendario />} />
-        <Route path="/resources" element={<Resources />} />
-        <Route path="/resources/alabanza" element={<Alabanza />} />
-        <Route path="/resources/predicacion" element={<Predicacion />} />
-        <Route path="/resources/evangelismo" element={<Evangelismo />} />
-        <Route path="/resources/multimedia" element={<Multimedia />} />
-        <Route path="/resources/deportes" element={<DeportesSociales />} />
-        <Route path="/conocenos" element={<Meet />} />
-        <Route path="/pasado" element={<ActivitiesPast />} />
-        <Route path="/materiales" element={<PdfSection />} />
-        <Route path="/cms2026" element={<Campamento />} />
-        <Route path="/cms2026/oficial" element={<CMS2026 />} />
-        <Route path="/cms2026/notas" element={<Notas />} /> */}
+
         <Route path="/evento-deportivo" element={<RegistroTorneo />} />
       </Routes>
     </Router>
